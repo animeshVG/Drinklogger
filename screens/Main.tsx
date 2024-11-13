0d8045419a420e3fbac73a303b7e60140da549aa
animeshVG <animesh.raikwar@vinzglobal.com>
2024-11-13 14:13:15 +0530

    Initialization

---
+++ screens/Main.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Set</Text>
+    </View>
+  );
+};
+
+export default Main;
+
+const styles = StyleSheet.create({});
---
+++ screens/Scan.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Scan = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default Scan;
+
+const styles = StyleSheet.create({});
